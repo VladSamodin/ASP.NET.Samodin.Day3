720eeddeb15e42a92add41ab4d48074ff53cb3eb
VladSamodin <samodin.vladislav@gmail.com>
2015-07-17 17:25:21 +0200

    init commit

---
+++ ASP.NET.Samodin.Day3/Polynom.cs
@@ -0,0 +1,241 @@
+﻿using System;
+
+namespace Task1.Polynom
+{
+    public sealed class Polynom : IEquatable<Polynom>, ICloneable
+    {
+        private readonly double[] coefficients;
+        private readonly int degree;
+
+        public int Degree
+        {
+            get
+            {
+                return degree;
+            }
+        }
+
+        public Polynom(): this(0, 0) { }
+
+        public Polynom(double coefficientMonomial, int degreeMonomial)
+        {
+            if (degreeMonomial < 0)
+            {
+                throw new ArgumentException("Degree monomial must be >= 0", "degreeMonomial");
+            }
+            if (coefficientMonomial == 0)
+            {
+                degreeMonomial = 0;
+            }
+            degree = degreeMonomial;
+            coefficients = new double[degreeMonomial + 1];
+            coefficients[degreeMonomial] = coefficientMonomial;
+        }
+
+        public Polynom(params double[] coefficients)
+        {
+            if (coefficients == null)
+            {
+                throw new ArgumentNullException("coefficients");
+            }
+            if (coefficients.Length == 0)
+            {
+                throw new ArgumentException("Coefficient array is empty", "coefficients");
+            }
+            degree = DetermineDegreePolynom(coefficients);
+            this.coefficients = new double[degree + 1];
+            Array.Copy(coefficients, this.coefficients, this.coefficients.Length);
+        }
+
+        public Polynom(Polynom toCopy)
+        {
+            if (toCopy == null)
+            {
+                throw new ArgumentNullException("toCopy");
+            }
+            degree = toCopy.Degree;
+            this.coefficients = toCopy.GetCoefficients();
+        }
+
+        public bool Equals(Polynom other)
+        {
+            if ((object)other == null)
+            {
+                return false;
+            }
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            if (this.degree != other.degree)
+            {
+                return false;
+            }
+            bool isEqual = true;
+            for (int i = 0; i <= this.degree; i++)
+            {
+                if (this[i] != other[i])
+                {
+                    isEqual = false;
+                    break;
+                }
+            }
+            return isEqual;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            Polynom polynom = obj as Polynom;
+            if ((object)polynom == null)
+            {
+                return false;
+            }
+            return this.Equals(polynom);
+        }
+
+        public object Clone()
+        {
+            //return new Polynom(this.coefficients);
+            return this;
+        }
+        
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            for (int i = 0; i <= Degree; i++)
+            {
+                byte[] data = BitConverter.GetBytes(this[i]);
+                int x = BitConverter.ToInt32(data, 0);
+                hash += x;
+                hash -= (hash << 13) | (hash >> 19);
+                int y = BitConverter.ToInt32(data, 4);
+                hash += y;
+                hash -= (hash << 13) | (hash >> 19);
+            }
+            return hash;
+        }
+
+        public double[] GetCoefficients()
+        {
+            return (double[])coefficients.Clone();
+        }
+
+        public double SubstituteVariableValue(double x)
+        {
+            double result = 0;
+            double currentPowX = 1;
+            for (int i = 0; i <= Degree; i++)
+            {
+                result += this[i] * currentPowX;
+                currentPowX *= x;
+            }
+            return result;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= coefficients.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return coefficients[index];
+            }
+        }
+
+        public static Polynom operator +(Polynom a, Polynom b)
+        {
+            return Add(a, b);
+        }
+
+        public static Polynom Add(Polynom a, Polynom b)
+        {
+            if (a.degree > b.degree)
+            {
+                SwapReferences(ref a, ref b);
+            }
+            int degeeResultPolynom = b.Degree;
+            double[] resultPolynomCoefficients = new double[degeeResultPolynom + 1];
+            for (int i = 0; i <= a.degree; i++)
+            {
+                resultPolynomCoefficients[i] = a[i] + b[i];
+            }
+            for (int i = a.degree + 1; i <= b.degree; i++)
+            {
+                resultPolynomCoefficients[i] = b[i];
+            }
+            return new Polynom(resultPolynomCoefficients);
+        }
+
+        public static Polynom operator -(Polynom a, Polynom b)
+        {
+            return Subtract(a, b);
+        }
+
+        public static Polynom Subtract(Polynom a, Polynom b)
+        {
+            return Add(a, Multiply(b, -1));
+        }
+
+        public static Polynom operator *(Polynom polynom, double factor)
+        {
+            return Multiply(polynom, factor);
+        }
+
+        public static Polynom Multiply(Polynom polynom, double factor)
+        {
+            if (factor == 0)
+            {
+                return new Polynom();
+            }
+            double[] resultPolynomCoefficients = new double[polynom.Degree + 1];
+            for (int i = 0; i <= polynom.Degree; i++)
+            {
+                resultPolynomCoefficients[i] = factor * polynom[i];
+            }
+            return new Polynom(resultPolynomCoefficients);
+        }
+
+        public static bool operator ==(Polynom a, Polynom b)
+        {
+            if (object.ReferenceEquals(a, b))
+            {
+                return true;
+            }
+            if ((object)a == null || (object)b == null)
+            {
+                return false;
+            }
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Polynom a, Polynom b)
+        {
+            return !(a == b);
+        }
+        
+        private int DetermineDegreePolynom(double[] coefficients)
+        {
+            int degree = coefficients.Length - 1;
+            for (; degree > 0; degree--)
+            {
+                if (coefficients[degree] != 0)
+                    break;
+            }
+            return degree;
+        }
+
+        private static void SwapReferences(ref Polynom a, ref Polynom b)
+        {
+            Polynom bufferRefence = a;
+            a = b;
+            b = bufferRefence;
+        }
+        
+    }
+}
---
+++ Task1.Polynom.Tests/PolynomTests.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Task1.Polynom;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Task1.Polynom.Tests
+{
+    [TestClass]
+    public class PolynomTests
+    {
+        [TestMethod]
+        public void Test_GetCoefficients()
+        {
+            double[] coefficients = new double[] { 1, 2, 3, 4, 5 };
+            Polynom a = new Polynom(coefficients);
+
+            IStructuralEquatable coeff = coefficients;
+            Assert.IsTrue(coeff.Equals(a.GetCoefficients(), StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Constructor_WithoutParameters_0()
+        {
+            Polynom a = new Polynom();
+
+            double[] expected = new double[] { 0 };
+
+            IStructuralEquatable actual = a.GetCoefficients();
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Constructor_Coefficietn2Degree5_MonomialDgree5()
+        {
+            Polynom a = new Polynom(2, 5);
+
+            double[] expected = new double[] { 0, 0, 0, 0, 0, 2 };
+
+            IStructuralEquatable actual = a.GetCoefficients();
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+         [TestMethod]
+        public void SubstituteVariableValue()
+        {
+             double[] coefficients = new double[] { 1, 2, 3, 4, 5 };
+             double x = 2;
+             Polynom a = new Polynom(coefficients);
+
+             double expected = 129;
+             double actual = a.SubstituteVariableValue(x);
+
+             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Equals_EquivalentPolynomials_True()
+        {
+            double[] coefficients = new double[] { 1, 2, 3, 4, 5 };
+            Polynom a = new Polynom(coefficients);
+            Polynom b = new Polynom(coefficients);
+
+            Assert.IsTrue(a.Equals(b));
+        }
+
+        public void Equals_InequivalentPolynomials_False()
+        {
+            double[] coefficientsA = new double[] { 1, 2, 3, 4, 5 };
+            double[] coefficientsB = new double[] { 1, 2, 3, 3, 5 };
+            Polynom a = new Polynom(coefficientsA);
+            Polynom b = new Polynom(coefficientsB);
+
+            Assert.IsFalse(a.Equals(b));
+        }
+
+        public void Equals_PolynomAndNull_False()
+        {
+            Polynom a = new Polynom();
+            Polynom b = null;
+
+            Assert.IsFalse(a.Equals(b));
+        }
+
+        
+        public void OperatorEquality_NullAndPolynom_False()
+        {
+            Polynom a = null;
+            Polynom b = new Polynom();
+
+            Assert.IsFalse(a == b);
+        }
+
+        public void OperatorEquality_NullAndNull_True()
+        {
+            Polynom a = null;
+            Polynom b = null;
+
+            Assert.IsTrue(a == b);
+        }
+
+        [TestMethod]
+        public void Test_Clone()
+        {
+            double[] coefficients = new double[] { 1, 2, 3, 4, 5 };
+            Polynom a = new Polynom(coefficients);
+            Polynom b = (Polynom)a.Clone();
+
+            Assert.IsTrue(a.Equals(b));
+        }
+
+        [TestMethod]
+        public void Add_PolynomsTheSameDegree_PolynomTheSameDegree()
+        {
+            double[] coefficientsA = new double[] { 1, 2, 3, 4, 5 };
+            double[] coefficientsB = new double[] { 4, 1, 8, 3, 5 };
+            Polynom a = new Polynom(coefficientsA);
+            Polynom b = new Polynom(coefficientsB);
+
+            double[] expected = new double[] { 5, 3, 11, 7, 10 };
+            IStructuralEquatable actual = (a + b).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Add_PolynomsDifferentDegrees_PolynomMaxDegree()
+        {
+            double[] coefficientsA = new double[] { 1, 2, 3, 4 };
+            double[] coefficientsB = new double[] { 4, 1, 8, 3, 5 };
+            Polynom a = new Polynom(coefficientsA);
+            Polynom b = new Polynom(coefficientsB);
+
+            double[] expected = new double[] { 5, 3, 11, 7, 5 };
+            IStructuralEquatable actual = (a + b).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Subtract_PolynomsTheSameDegree_PolynomTheSameDegree()
+        {
+            double[] coefficientsA = new double[] { 1, 2, 3, 4, 6 };
+            double[] coefficientsB = new double[] { 4, 1, 8, 3, 5 };
+            Polynom a = new Polynom(coefficientsA);
+            Polynom b = new Polynom(coefficientsB);
+
+            double[] expected = new double[] { -3, 1, -5, 1, 1 };
+            IStructuralEquatable actual = (a - b).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Subtract_PolynomsDifferentDegrees_PolynomMaxDegree()
+        {
+            double[] coefficientsA = new double[] { 1, 2, 3, 4 };
+            double[] coefficientsB = new double[] { 4, 1, 8, 3, 5 };
+            Polynom a = new Polynom(coefficientsA);
+            Polynom b = new Polynom(coefficientsB);
+
+            double[] expected = new double[] { -3, 1, -5, 1, -5 };
+            IStructuralEquatable actual = (a - b).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Subtract_PolynomsWithEqualCoefficientMaxDegree_PolynomLessDegree()
+        {
+            double[] coefficientsA = new double[] { 1, 2, 3, 4, 5 };
+            double[] coefficientsB = new double[] { 4, 1, 8, 3, 5 };
+            Polynom a = new Polynom(coefficientsA);
+            Polynom b = new Polynom(coefficientsB);
+
+            double[] expected = new double[] { -3, 1, -5, 1 };
+            IStructuralEquatable actual = (a - b).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Multiply_PolynomBy2_DoubledCoefficients()
+        {
+            double[] coefficients = new double[] { 1, 2, 3, 4, 5 };
+            Polynom a = new Polynom(coefficients);
+
+            double[] expected = new double[] { 2, 4, 6, 8, 10 };
+            IStructuralEquatable actual = (a * 2).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+
+        [TestMethod]
+        public void Multiply_PolynomByMinus1_InvertedSignsCoefficients()
+        {
+            double[] coefficients = new double[] { 1, 2, 3, 4, 5 };
+            Polynom a = new Polynom(coefficients);
+
+            double[] expected = new double[] { -1, -2, -3, -4, -5 };
+            IStructuralEquatable actual = (a * (-1)).GetCoefficients();
+
+            Assert.IsTrue(actual.Equals(expected, StructuralComparisons.StructuralEqualityComparer));
+        }
+    }
+}
